0949426d17d2f19d12eda76fdd99dd6bde79621f
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-04-01 19:55:01 -0300

    feat: create interface to manage heap

--- Program.cs
+++ Program.cs
@@ -4,9 +4,54 @@ namespace heaps
 {
     class Program
     {
+        static Heap heap = new Heap(10);
+
+        static string ask(string msg) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine();
+            System.Console.WriteLine(msg);
+            Console.ResetColor();
+            return Console.ReadLine();
+        }
+
+        static void addNumber(int n) {
+            heap.add(n);
+        }
+
+        static void pop() {
+            var n = heap.pop();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine($"* Popped {n}");
+            Console.ResetColor();
+        }
+
+        static void print() {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.WriteLine();
+            System.Console.WriteLine("********** Here's your Heap:");
+            Console.ResetColor();
+            heap.debug();
+        }
+
         static void Main(string[] args)
         {
-            var heap = new Heap(10);
+            string input;
+            int addN;
+
+            do {
+                input = ask("What value you want to add? (Type bare number to add, tap - to pop)");
+                bool isNumeric = int.TryParse(input, out addN);
+
+                if (isNumeric) {
+                    addNumber(addN);
+                }
+                else if (input == "-") {
+                    pop();
+                }
+
+                print();
+
+            } while (input != "");
 
             for (var i = 1; i <= 10; i++) {
                 heap.add(i);
